111d34905a03d82b9d0ac85360b73cb016867c68
kauefraga <kauefragarodrigues456@gmail.com>
2022-10-01 21:25:09 -0300

    :truck: move: change project structure

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -4,9 +4,8 @@ import Quicksort from './Quicksort'
 import BinarySearch from './BinarySearch'
 
 const array = [1, 3, 4, 6, 8, 9, 20, 34, 41, 54, 67, 212, 434, 501, 502, 1002, 3403, 5000, 5001, 5002, 5003, 5004]
-// const arrayOdd = [0, 2, 3, 4, 6, 7, 8, 10, 11, 22, 33, 43, 54]
 
-const target = 8 || 5002
+const target = 9
 
 try {
   console.log(
@@ -16,7 +15,7 @@ try {
   )
 } catch (error) {
   console.log(
-    chalk.yellow(
+    chalk.red(
       error
     )
   )
